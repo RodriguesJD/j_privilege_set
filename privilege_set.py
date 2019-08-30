134d4e62e4f8aa733cb4db3c7ba5978e5a4215e7
Jeremy Rodrigues <jeremy.rodrigues@therealreal.com>
2019-08-30 14:18:49 -0700

    xml_accoun_info() handles the xml before the privleges portion.

--- privilege_set.py
+++ privilege_set.py
@@ -1,21 +1,6 @@
 from tool_box import tools
-from xml.etree import ElementTree
 
-ET = ElementTree
+
+
 xml_text = tools.get_accounts_xml().text
-xml_str = tools.xml_str(xml_text=xml_text)
-privilege_set = xml_str
-print(privilege_set)
-print("\n\n\n")
-header_priv_set = f"{privilege_set.split('</access_level>')[0]}</access_level>"
-
-privilege_set_xml = "    <privilege_set>test_set</privilege_set>"
-
-privileges = f"    <privileges>{privilege_set.split('<privileges>')[1].split('</privileges>')[0]}</privileges>"
-
-footer_priv_set = "</account>"
-
-put_xml_string = f"{header_priv_set}\n{privilege_set_xml}\n{privileges}\n{footer_priv_set}"
-
-
-# TODO put the put_xml_string and see if that changes the name of the <privilege_set></privilege_set> if so check webUI
+print(tools.xml_account_info("Custom", xml_text))
--- tool_box/tools.py
+++ tool_box/tools.py
@@ -22,11 +22,31 @@ def get_accounts_xml():
     return req
 
 
+def put_trr_jamf(put_xml: str):
+    return requests.put(f'{base_url}/accounts/userid/{user_id}',
+                        auth=HTTPBasicAuth(username, key), headers={'Content-Type': 'application/xml'}, data=put_xml)
+
+
 def xml_str(xml_text):
     dom = xml.dom.minidom.parseString(xml_text)
     pretty_xml_as_string = dom.toprettyxml()
 
     return pretty_xml_as_string
+
+
+def xml_account_info(privilege_set, xml_text):
+    """
+    Parse xml_text and pass in the privilage_set var. Privilege set must be an excising option.
+    :param privilege_set:
+    :param xml_text:
+    :return:
+    """
+    xml_account_info_header = xml_text.split('<privilege_set>')[0]
+
+    priv_set = f"<privilege_set>{privilege_set}</privilege_set>"
+
+    return xml_account_info_header + priv_set
+
 
 
 
