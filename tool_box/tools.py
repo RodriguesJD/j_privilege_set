c0e49515c1f2f515b47e845a686d523dec922b8a
Jeremy Rodrigues <jeremy.rodrigues@therealreal.com>
2019-09-02 15:58:38 -0700

    Added type to comment.

--- tool_box/tools.py
+++ tool_box/tools.py
@@ -103,7 +103,7 @@ def xml_user_info(privilege_set: str, xml_text: str) -> Union[None, str]:
 def make_user_admin(xml_text):
     """
     Grant user admin rights.
-    :param xml_text:
+    :param xml_text: str
     :return: int
     """
     privilege_set = 'Administrator'
